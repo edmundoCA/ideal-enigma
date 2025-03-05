1fb4b91819c8966922d52edcb96949d6a9503349
Edmundo Corona <113280885+edmundoCA@users.noreply.github.com>
2025-03-05 03:43:55 +0000

    fix cicd

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,6 +1,14 @@
 dotnet restore
 
 echo "Installing Google Chrome..."
+sudo apt update
+sudo apt-get upgrade
+
+sudo apt-get install -y libxss1 libappindicator3-1 libindicator7 fonts-liberation libdbus-1-3
+
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install ./google-chrome-stable_current_amd64.deb -y
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+google-chrome-stable --version
+
 echo "Google Chrome installed successfully"

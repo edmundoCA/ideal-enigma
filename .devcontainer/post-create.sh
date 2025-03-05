309178b869e619d2e87497631a69446d386e6080
Edmundo Corona <113280885+edmundoCA@users.noreply.github.com>
2025-03-05 03:26:24 +0000

    added post create script

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,6 @@
+dotnet restore
+
+echo "Installing Google Chrome..."
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb -y
+echo "Google Chrome installed successfully"

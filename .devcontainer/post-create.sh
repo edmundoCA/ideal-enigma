a10919818a08f069afe911271786c863a40eb880
Edmundo Corona <113280885+edmundoCA@users.noreply.github.com>
2025-03-05 03:55:02 +0000

    fixed prompt

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -2,13 +2,11 @@ dotnet restore
 
 echo "Installing Google Chrome..."
 sudo apt update
-sudo apt-get upgrade
+sudo apt-get upgrade -y
 
 sudo apt-get install -y libxss1 libappindicator3-1 libindicator7 fonts-liberation libdbus-1-3
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 
-google-chrome-stable --version
-
-echo "Google Chrome installed successfully"
+google-chrome-stable --version && echo "✅ Google Chrome installed successfully"
